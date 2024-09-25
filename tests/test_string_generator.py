4f8bd72f3d370d0a77b7821b8f5c67bcb4335914
jonnyell89 <jonnyell89@gmail.com>
2024-09-25 23:07:21 +0100

    final commit before archive

--- tests/test_string_generator.py
+++ tests/test_string_generator.py
@@ -1,11 +1,12 @@
 import pytest
 
-from config.config import FRETBOARD_LEN
-from app.string_generator import StringGenerator
+from config.config import CHROMATIC_SCALE, FRETBOARD_LEN
+from archive.string_generator import StringGenerator
 
-def test_string_generator_init():
+@pytest.mark.parametrize("root_note", CHROMATIC_SCALE)
+def test_string_generator_init(root_note):
 
-    string = StringGenerator("E")
+    string = StringGenerator(root_note=root_note)
 
     assert string.string_len == FRETBOARD_LEN
 
